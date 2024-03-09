c768950645f85d93e47e5a32bc7cd5be629554f0
FerDesaM <fdezaso@unsa.edu.pe>
2024-03-09 17:28:21 -0500

    Change code create new function play game for an better presentation

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let PlayerChoice;
 function getComputerChoice(){
     let opcion=["ROCK","PAPER","SCISOR"];
     let random=Math.floor(Math.random()*opcion.length);
@@ -7,49 +8,52 @@ const paper=document.querySelector("#paper");
 const rock=document.querySelector("#rock");
 const scisor=document.querySelector("#scisor");
 paper.addEventListener("click",()=>{
-    Choice="paper";
-    PlayerChoice=Choice.toUpperCase();
+    PlayerChoice="PAPER";
+    PlayGame()
 });
 rock.addEventListener("click",()=>{
-    Choice="rock";
-    PlayerChoice=Choice.toUpperCase();
-}
-);
-scisor.addEventListener("click",()=>{
-    Choice="scisor";
-    PlayerChoice=Choice.toUpperCase();
+    PlayerChoice="ROCK";
+    PlayGame();
 });
-
+scisor.addEventListener("click",()=>{
+    PlayerChoice="SCISOR";
+    PlayGame();
+});
 function WinLoser(ComputerChoice,PlayerChoice){
-    let a;
     if(ComputerChoice==="ROCK"&&PlayerChoice==="scisor"){
-        console.log("You Lose! Rock beats scisor");
         return 1;
     }
       else if(ComputerChoice==="PAPER"&&PlayerChoice==="ROCK"){
-        console.log("You Lose! Paper beats rock ");
         return 1;
     }
       else if(ComputerChoice==="SCISOR"&&PlayerChoice==="PAPER"){
-        console.log("You Lose! Scisor beats paper");
         return 1;
     }
       else if(ComputerChoice==="ROCK"&&PlayerChoice==="PAPER"){
-        console.log("YOu WIn! Paper beats Rock");
         return 2;
     }
       else if(ComputerChoice==="PAPER"&&PlayerChoice==="SCISOR"){
-        console.log("YOu WIn! SCISOR beats Paper");
         return 2;
     }
       else if(ComputerChoice==="SCISOR"&&PlayerChoice==="ROCK"){
-        console.log("YOu WIn! Rock beats scisor");
         return 2;
     }else{
-        console.log("Choose another opcion");
     }   
 }
-
+function PlayGame(){
+const ComputerChoice=getComputerChoice();
+console.log("Computer Choice: ",ComputerChoice);
+console.log("Player Choice: ",PlayerChoice);
+let result=WinLoser(ComputerChoice,PlayerChoice);
+    if(result===1){
+        alert("You Lose! "+ComputerChoice+" beats "+PlayerChoice);
+    }
+    else if(result===2){
+        alert("You Win! "+ PlayerChoice +" beats "+ComputerChoice);
+    }else{
+        alert("Its a tie");
+    } 
+}
 
 
 
